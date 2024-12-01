7f3d6adaacb6b18247c4fa5f4832fe419fd2dec7
Prabhu kumar <prabhukumarmb@gmail.com>
2024-12-01 22:41:12 +0530

    altered the styles of user login

--- frontend/src/Users/UserloginNavbar.jsx
+++ frontend/src/Users/UserloginNavbar.jsx
@@ -20,16 +20,22 @@ const UserloginNavbar = () => {
     <div>
              <AppBar position="static" sx={{ backgroundColor: '#1976d2' }}>
         <Toolbar>
-          <Typography variant="h6" sx={{ flexGrow: 1 , fontSize: {xs:"18px", sm: "22px", lg: "28px"}, fontWeight:{lg:"600", sm: "600", xs: "500"}}}>
+          <Typography variant="h6" sx={{ flexGrow: 1 , fontSize: {xs:"20px", sm: "22px", lg: "28px"}, fontWeight:{lg:"600", sm: "600", xs: "500"}}}>
             User Login
           </Typography>
-          <Button color="inherit" onClick={() => navigate('/')}>
+          <Button color="inherit" onClick={() => navigate('/')}
+            sx={{fontSize: {xs:"16px", sm: "18px", lg: "21px"}, fontWeight:{lg:"600", sm: "600", xs: "500"}}}
+            >
             Home
           </Button>
-          <Button color="inherit" onClick={() => navigate('/contact')}>
+          <Button color="inherit" onClick={() => navigate('/contact')}
+            sx={{fontSize: {xs:"16px", sm: "18px", lg: "21px"}, fontWeight:{lg:"600", sm: "600", xs: "500"}}}
+            >
             Contact Us
           </Button>
-          <Button color="inherit" onClick={() => navigate('/user')}>
+          <Button color="inherit" onClick={() => navigate('/user')}
+            sx={{fontSize: {xs:"16px", sm: "18px", lg: "21px"}, fontWeight:{lg:"600", sm: "600", xs: "500"}}}
+            >
             Registration
           </Button>
         </Toolbar>
