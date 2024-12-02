6b19d4362a1b3dbafc43cdafae1ece048d9dddb5
Prabhu kumar <prabhukumarmb@gmail.com>
2024-12-02 11:14:17 +0530

    removed comments

--- backend/Authentication/TrustUserAuth.js
+++ backend/Authentication/TrustUserAuth.js
@@ -5,14 +5,12 @@ const TrustModel = require('../models/Trust.model')
 const trustUserAuth = async (req, res, next)=>{
 try{
         const {authToken} = req.cookies
-        // console.log(authToken)
         if(!authToken){
             throw new Error("invalid User please login")
         }
        let decodedData = jwt.verify(authToken, process.env.JWT_SECREAT_KEY)
         
         if(decodedData){
-            // console.log(decodedData)
            let user = await TrustModel.findOne({_id: decodedData._id})
 
             if(!user){
